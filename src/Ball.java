30e63befe14dbdcd59e5e2f07f89bbfa5d9b8607
nextechcatapult <rubybarrera77@gmail.com>
2020-02-05 18:19:39 -0500

    Wednesday-Ball y Player Moves + Collisions

--- src/Ball.java
+++ src/Ball.java
@@ -4,18 +4,22 @@ public class Ball {
 
     int x, y;
     final int DIAMETER = 30;
-    double dx = 5, dy = 7;
+    final int SPEED = 5;
+
+    double dx = SPEED, dy = SPEED;
+
+    double angle = 5*Math.PI/12;
 
     Board board;
 
     public Ball(Board board){
         x = (board.getWidth()/2) - DIAMETER/2;
         y = board.getHeight() - (DIAMETER + 100);
+
+        this.board = board;
     }
 
     public void move(){
-
-        //LEFT AND RIGHT
         if(x < 0){
             dx*=-1;
         }
@@ -30,10 +34,47 @@ public class Ball {
         y+=dy;
     }
 
+    public Rectangle getBounds(){
+        return new Rectangle(x, y, DIAMETER, DIAMETER);
+    }
+
+    public void checkCollisions(Player p){
+        if(getBounds().intersects(p.getBounds())){
+            double player = p.getBounds().getX();
+            double ballx = x;
+
+            if(x + (DIAMETER/2) < player){
+                ballx = x + DIAMETER;
+            } else if (x + (DIAMETER/2) > player + p.getBounds().getWidth()){
+                ballx = x;
+            } else {
+                ballx = x + DIAMETER;
+
+                if(x < board.getHeight()/2){
+                    dy*=-1;
+                }
+                if(x > (board.getHeight()/2)){
+                    dy*=-1;
+                }
+            }
+        }
+
+    }
+
     public void paint(Graphics g){
         g.setColor(Color.WHITE);
         g.fillOval(x, y, DIAMETER, DIAMETER);
     }
 
+    public int getX() {
+        return x;
+    }
 
+    public int getY() {
+        return y;
+    }
+
+    public int getDiam(){
+        return DIAMETER;
+    }
 }
--- src/Board.java
+++ src/Board.java
@@ -13,11 +13,14 @@ public class Board extends JPanel implements ActionListener{
     Ball ball;
     Brick brick;
     Brick[][] bricks = new Brick[7][15];
+    Timer timer;
 
     public Board(Game game){
         this.game = game;
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.PINK);
+        timer = new Timer(1000/60, this);
+        timer.start();
     }
 
     public void setup(){
@@ -30,12 +33,20 @@ public class Board extends JPanel implements ActionListener{
         }
     }
 
-    public int getWIDTH() {
-        return WIDTH;
+    public void checkCollisions(){
     }
 
-    public int getHEIGHT() {
-        return HEIGHT;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(game.isLeftPressed() && player.getX() > 0){
+            player.moveLeft();
+        }
+        if(game.isRightPressed() && player.getX()+player.getWIDTH() < getWidth()){
+            player.moveRight();
+        }
+        ball.checkCollisions(player);
+        ball.move();
+        repaint();
     }
 
     @Override
@@ -49,11 +60,5 @@ public class Board extends JPanel implements ActionListener{
             }
         }
 
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        ball.move();
-        repaint();
     }
 }
